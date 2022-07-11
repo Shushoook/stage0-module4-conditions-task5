311c4913f3de11dc8283bdb38e52eac47f5febde
Shushoook <shushoook666@gmail.com>
2022-07-11 22:52:27 +0400

    stage0-module4-conditions-task5

--- src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
+++ src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
@@ -3,6 +3,7 @@ package school.mjc.stage0.conditions.task5;
 public class BasicSwitch {
     public static void main(String[] args) {
         int a = 20;
+        a = 25;
         switch (a) {
             default:
                 System.out.println("default");
